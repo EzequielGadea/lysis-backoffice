700fa4f2bac9dfa4737865d7afb83a3287716e91
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-09-16 02:46:58 -0300

    ruta web para login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('login');
+})->name('login');
+
+Route::get('menu', function () {
     return view('index');
-});
+})->middleware('auth');
