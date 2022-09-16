6f995eaf3611d7e265815b5bdcbc7c26af6beed9
RodriGarcia12 <rgarciaa125@gmail.com>
2022-09-16 00:56:24 -0300

    menu para gestionar funciones

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/output.css">
+    <title>Backoffice | Home</title>
+</head>
+<body>
+    <main class="flex flex-row item-center justify-center p-5">
+        <div class="w-1/3 flex flex-col justify-center gap-2 bg-gray-100 rounded-xl p-4 pb-4 shadow-xl shadow-gray-300 mt-10">
+            <h1 class="w-100 text-xl m-auto">Menú:</h1>
+            <a href="#">
+                <p class="text-md p-2 text-black hover:text-gray-500">Gestion de usuarios de aplicacion publica</p>
+            </a>
+            <a href="#">
+                <p class="text-md p-2 text-black hover:text-gray-500">Gestion de usuarios de aplicacion de administracion</p>
+            </a>
+            <a href="#">
+                <p class="text-md p-2 text-black hover:text-gray-500">Gestion de banners de publicidad</p>
+            </a>
+        </div>
+    </main>
+</body>
+</html>
