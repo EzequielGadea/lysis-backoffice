4ad9e5e57d81f70adee8cbc4a9f98f3bf51df983
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-09-18 21:10:55 -0300

    eliminacion route api

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,3 @@ use App\Http\Controllers\UserController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('login', [UserController::class, 'Authenticate']);
