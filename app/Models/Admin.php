4e35b506de388a21411f9e365d26d5d4cd2c91ad
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-09-20 19:15:00 -0300

    uso de soft deletes

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,13 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
     
     /**
      * The attributes that are mass assignable.
