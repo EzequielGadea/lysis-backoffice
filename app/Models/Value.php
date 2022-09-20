3cbc183e0d9a440b82a067ae14ff3945fbd05569
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-09-20 19:15:41 -0300

    uso de soft deletes

--- app/Models/Value.php
+++ app/Models/Value.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Tag;
 use App\Models\Ad;
 
 class Value extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'value',
