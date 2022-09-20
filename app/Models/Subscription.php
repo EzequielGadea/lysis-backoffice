f15894c2cea0068e3b164009aaea0f3d2d176b5b
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-09-20 19:15:14 -0300

    uso de soft deletes

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\SubscriptionType;
 use App\Models\Client;
 
 class Subscription extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     public function user() {
         return $this->hasMany(Client::class);
