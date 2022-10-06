be58924a3da55c0ae39ba8204ef402203158445c
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-06 20:38:22 -0300

    merge para poder agregar clases desde otras vistas

--- resources/views/components/update-card.blade.php
+++ resources/views/components/update-card.blade.php
@@ -1,7 +1,13 @@
-<form action="/{{ $action }}" method="post" class="flex flex-col items-start gap-5 p-0 pt-6 rounded-md bg-white overflow-hidden shadow-md w-fit m-auto mt-5">
+<form action="/{{ $action }}" method="post" {{ $attributes->merge(['class' => 'flex flex-col items-start gap-5 p-0 pt-6 rounded-md bg-white shadow-md w-fit m-auto mt-5 mb-5']) }}>
     <p class="text-2xl font-semibold text-zinc-800 px-6 w-full">Edit</p>
     @csrf
-    {{ $slot }}
+    <div class="flex flex-col items-start px-6 gap-2 flex-wrap">
+        {{ $slot }}
+    </div>
+    <div class="flex flex-row justify-end items-center py-3 px-6 bg-slate-200 w-full gap-10">
+        <a class="text-zinc-800 hover:cursor-pointer" id="cancel">Cancel</a>
+        <button class="bg-blue-600 text-white px-3 py-1 w-fit rounded-md" type="submit">Save</button>
+    </div>
 </form>
 <form method="GET" action="/{{ $backTo }}" id="redirect">
     @csrf
