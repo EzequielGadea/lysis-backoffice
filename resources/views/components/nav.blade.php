2d06c7a892568e10973cf39b29c59d54c0433da8
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-07 00:04:18 -0300

    nuevo enlace para gestion de tags

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="flex flex-col pl-8 pr-3 pt-6 border-r-2 border-slate-500 min-h-screen w-48 justify-between">
+<nav class="flex flex-col pl-8 pr-3 pt-6 border-r-2 border-slate-500 min-h-screen w-48">
     <div class="flex flex-col gap-5">
         <form action="/userManagement" method="GET">
             @csrf
@@ -12,8 +12,12 @@
             @csrf
             <button class="font-medium text-zinc-800" id="nav-ads" type="submit">Ads</button>
         </form>
+        <form action="/tagManagement" method="GET">
+            @csrf
+            <button class="font-medium text-zinc-800" id="nav-tags" type="submit">Tags</button>
+        </form>
     </div>
-    <form action="logout" method="POST">
+    <form action="logout" method="POST" class="mt-12">
         @csrf
         <button type="submit">Log out</button>
     </form>
