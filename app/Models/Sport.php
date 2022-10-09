bc0e4061ed0714c6050135c2a99ec2d03e8d8d85
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-09 01:42:17 -0300

    modelo para tabla sports

---
+++ app/Models/Sport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\League;
+
+class Sport extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'logo_link'
+    ];
+
+    public function leagues()
+    {
+        return $this->hasMany(League::class);
+    }
+}
