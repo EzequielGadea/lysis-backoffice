209640f4ef89f9a5b867478ac6d4c46553efdac6
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-09 01:42:03 -0300

    modelo para tabla manager

---
+++ app/Models/Manager.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Country;
+use App\Models\Team;
+
+class Manager extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'surname',
+        'birth_date'
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function team()
+    {
+        return $this->hasOne(Team::class);
+    }
+}
