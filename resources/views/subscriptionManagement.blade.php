3732d167eb485264b2780e3e8d163dbd00a40095
RodriGarcia12 <rgarciaa125@gmail.com>
2022-10-11 16:27:46 -0300

    divs de espaciado

--- resources/views/subscriptionManagement.blade.php
+++ resources/views/subscriptionManagement.blade.php
@@ -60,9 +60,13 @@
             <label for="type" class="font-medium text-zinc-700">Type</label>
             <input type="text" name="type" id="type" placeholder="Enter type" value="{{ old('type') }}" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner">
             <p class="text-sm text-red-600">{{ $errors->first('type') }}</p>
+        </div>
+        <div class="flex flex-col gap-1">
             <label for="price" class="font-medium text-zinc-700">Price</label>
             <input type="number" min="0" name="price" id="price" placeholder="Enter price" value="{{ old('price') }}" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner">
             <p class="text-sm text-red-600">{{ $errors->first('price') }}</p>
+        </div>
+        <div class="flex flex-col gap-1">
             <label for="description" class="font-medium text-zinc-700">Description</label>
             <input type="text" name="description" id="description" placeholder="Enter description" value="{{ old('description') }}" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner">
             <p class="text-sm text-red-600">{{ $errors->first('description') }}</p>
