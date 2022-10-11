9c7a3980cbf9240ca887f97dce49d5cafa1f5dd5
RodriGarcia12 <rgarciaa125@gmail.com>
2022-10-11 16:26:32 -0300

    campo country_id en fillable

--- app/Models/Events/Referee.php
+++ app/Models/Events/Referee.php
@@ -14,7 +14,8 @@ class Referee extends Model
     protected $fillable = [
         'name',
         'surname',
-        'birth_date'
+        'birth_date',
+        'country_id'
     ];
 
     public function country()
