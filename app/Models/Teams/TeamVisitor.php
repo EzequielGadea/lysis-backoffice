0c68ac2166dc4a26827d5d2ee6ad4b2952841dc9
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-21 17:55:53 -0300

    Modelos para relación entre Event y Team(s) - Uso de SoftDeletes - Especificación de nombre de tabla y campos asignables en masa - Métodos para su relacion con Event y Team(s)

---
+++ app/Models/Teams/TeamVisitor.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Teams;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Events\Event;
+use App\Models\Teams\Team;
+
+class TeamVisitor extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'team_visitor';
+
+    protected $fillable = [
+        'event_id',
+        'player_id'
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+}
