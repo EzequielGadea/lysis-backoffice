e3da6ce3aed8fbfe6df649ec2bea2bfc8df7b498
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-23 20:00:57 -0300

    Componente para inputs dependientes de isIndividual En caso de que el deporte sea individual, se actualiza el DOM para cambiar los select's

--- app/Http/Livewire/CreateEventForm.php
+++ app/Http/Livewire/CreateEventForm.php
@@ -3,11 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Players\Player;
+use App\Models\Teams\Team;
 
 class CreateEventForm extends Component
 {
+    public $isIndividual;
+
+    public $chosenLocalPlayer;
+
+    public $chosenVisitorPlayer;
+
+    public $chosenLocalTeam;
+
+    public $chosenVisitorTeam;
+
     public function render()
     {
-        return view('livewire.create-event-form');
+        if($this->isIndividual)    
+            return view('livewire.create-event-form', [
+                'localPlayers' => Player::all()->except($this->chosenVisitorPlayer),
+                'visitorPlayers' => Player::all()->except($this->chosenLocalPlayer)
+            ]);
+
+        return view('livewire.create-event-form', [
+            'localTeams' => Team::all()->except($this->chosenVisitorTeam),
+            'visitorTeams' => Team::all()->except($this->chosenLocalTeam)
+        ]);
     }
 }
