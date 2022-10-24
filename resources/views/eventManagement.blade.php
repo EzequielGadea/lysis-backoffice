3a2472eea3192081fc7e1d4ea76d2975276df38f
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-23 22:05:03 -0300

    Nuevos campos en tabla y form de creacion - Campos de tabla y operadores en caso de que no tenga ese campo - inputs para crear evento

--- resources/views/eventManagement.blade.php
+++ resources/views/eventManagement.blade.php
@@ -1,30 +1,46 @@
 <x-layout>
     <x-slot name="title">Event management</x-slot>
     <x-nav/>
-    <div class="flex flex-col items-center pt-6 px-8 w-full flex-grow">
+    <div class="flex flex-col items-center pt-6 px-8 w-[50rem] flex-grow">
         <p class="text-2xl text-zinc-800 font-semibold mb-6 w-full">Events</p>
         <div class="rounded-md overflow-x-auto shadow-xl w-full">
-            <table class="table-auto border-collapse w-full">
+            <table class="table-auto border-collapse whitespace-nowrap w-full">
                 <thead>
                     <tr class="bg-slate-300">
-                        <td class="pl-3 py-3 font-light text-zinc-800">EVENT ID</td>
-                        <td class="py-3 font-light text-zinc-800">NAME</td>
-                        <td class="py-3 font-light text-zinc-800">VENUE</td>
-                        <td class="py-3 font-light text-zinc-800">CREATED AT</td>
-                        <td class="py-3 font-light text-zinc-800">UPDATED AT</td>
-                        <td class="pr-3 py-3 font-light text-zinc-800 text-center">ACTIONS</td>
+                        <td class="p-3 font-light text-zinc-800">ID</td>
+                        <td class="p-3 font-light text-zinc-800">START DATE</td>
+                        <td class="p-3 font-light text-zinc-800">VENUE</td>
+                        <td class="p-3 font-light text-zinc-800">OPPONENTS</td>
+                        <td class="p-3 font-light text-zinc-800">LEAGUE</td>
+                        <td class="p-3 font-light text-zinc-800">SPORT</td>
+                        <td class="p-3 font-light text-zinc-800">CREATED AT</td>
+                        <td class="p-3 font-light text-zinc-800">UPDATED AT</td>
+                        <td class="p-3 font-light text-zinc-800 text-center">ACTIONS</td>
                     </tr>
                 </thead>
                 <tbody>
                     @if (isset($events))
                         @foreach ($events as $event)
                             <tr class="border-b border-slate-300">
-                                <td class="pl-3 text-zinc-800">{{ $event->id }}</td>
-                                <td class="py-3 text-zinc-800">{{ $event->start_date }}</td>
-                                <td class="py-3 text-zinc-800">{{ $event->venue->name }}</td>
-                                <td class="py-3 text-zinc-800">{{ $event->created_at }}</td>
-                                <td class="py-3 text-zinc-800">{{ $event->updated_at }}</td>
-                                <td class="pr-3 text-zinc-800">
+                                <td class="p-3 text-zinc-800 text-right">{{ $event->id }}</td>
+                                <td class="p-3 text-zinc-800">{{ $event->start_date }}</td>
+                                <td class="p-3 text-zinc-800">{{ $event->venue->name }}</td>
+                                <td class="p-3 text-zinc-800 text-center">
+                                    <p>
+                                        {{ $event->playerVisitor->player->name ?? $event->teamVisitor->team->name ?? ''}}
+                                        {{ $event->playerVisitor ? $event->playerVisitor->player->surname : '' }}
+                                    </p>
+                                    <p class="font-semibold text-zinc-800">vs</p>
+                                    <p>
+                                        {{ $event->playerLocal->player->name ?? $event->teamLocal->team->name ?? '' }}
+                                        {{ $event->playerLocal ? $event->playerLocal->player->surname : '' }}
+                                    </p>
+                                </td>
+                                <td class="p-3 text-zinc-800">{{ $event->league->first->name->name ?? 'Not in league' }}</td>
+                                <td class="p-3 text-zinc-800">{{ $event->league->first->sport->sport->name ?? 'Not in sport' }}</td>
+                                <td class="p-3 text-zinc-800">{{ $event->created_at }}</td>
+                                <td class="p-3 text-zinc-800">{{ $event->updated_at }}</td>
+                                <td class="p-3 text-zinc-800">
                                     <div class="flex flex-col items-center">
                                         <form action="eventUpdate/{{ $event->id }}" method="GET">
                                             <button class="font-semibold text-blue-600">Edit</button>
@@ -52,31 +68,33 @@
             <x-status-restore/>
         @endif
     </div>
-    {{-- <x-create-section>
+    <x-create-section>
         <x-slot name="action">eventRegister</x-slot>
         <div class="flex flex-col gap-1">
-            <label for="name" class="font-medium text-zinc-700">Name</label>
-            <input type="text" name="name" id="name" placeholder="Enter name" value="{{ old('name') }}" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner">
-            <p class="text-sm text-red-600">{{ $errors->first('name') }}</p>
+            <label for="startDate" class="font-medium text-zinc-700">Start date</label>
+            <input type="datetime-local" name="startDate" id="startDate" value="{{ old('startDate') }}" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner">
+            <p class="text-sm text-red-600">{{ $errors->first('startDate') }}</p>
         </div>
         <div class="flex flex-col gap-1">
-            <label for="surname" class="font-medium text-zinc-700">Surname</label>
-            <input type="text" name="surname" id="surname" placeholder="Enter surname" value="{{ old('surname') }}" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner">
-            <p class="text-sm text-red-600">{{ $errors->first('surname') }}</p>
-        </div>
-        <div class="flex flex-col gap-1">
-            <label for="birthDate" class="font-medium text-zinc-700">Birth date</label>
-            <input type="date" name="birthDate" id="birthDate" placeholder="Enter birth date" value="{{ old('birthDate') }}" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner">
-            <p class="text-sm text-red-600">{{ $errors->first('birthDate') }}</p>
-        </div>
-        <div class="flex flex-col gap-1">
-            <label for="country" class="font-medium text-zinc-700">Country</label>
-            <select name="countryId" id="country" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner" autocomplete="off">
-                @foreach($countries as $country)
-                    <option value="{{ $country->id }}">{{ $country->name }}</option>
+            <label for="leagueId" class="font-medium text-zinc-700">League</label>
+            <select name="leagueId" id="leagueId" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner">
+                <option value="" selected>Doesn't belong to league</option>
+                @foreach ($leagues as $league)
+                    <option value="{{ $league->id }}">{{ $league->name }}</option>
                 @endforeach
             </select>
-            <p class="text-sm text-red-600">{{ $errors->first('country') }}</p>
+            <p class="text-sm text-red-600">{{ $errors->first('leagueId') }}</p>
         </div>
-    </x-create-section> --}}
+        <div class="flex flex-col gap-1">
+            <label for="venueId" class="font-medium text-zinc-700">Venue</label>
+            <select name="venueId" id="venueId" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner">
+                <option selected disabled>Choose venue</option>
+                @foreach ($venues as $venue)
+                    <option value="{{ $venue->id }}">{{ $venue->name }}</option>
+                @endforeach
+            </select>
+            <p class="text-sm text-red-600">{{ $errors->first('venueId') }}</p>
+        </div>
+        @livewire('create-event-form')
+    </x-create-section>
 </x-layout>
