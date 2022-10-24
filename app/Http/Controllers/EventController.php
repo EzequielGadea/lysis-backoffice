8cfa1ab8c91f804b6eaba4ae679a29c3b23ec233
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-23 22:01:02 -0300

    crud eventos Falta probar que todo funcione, probablemente no funcione

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,12 +6,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use App\Models\Events\Event;
+use App\Models\Common\League;
+use App\Models\Whereabouts\Venue;
 use App\Models\Players\PlayerVisitor;
 use App\Models\Players\PlayerLocal;
 use App\Models\Teams\TeamVisitor;
 use App\Models\Teams\TeamLocal;
 use App\Http\Requests\EventController\EventCreateRequest;
 use App\Http\Requests\EventController\EventUpdateRequest;
+use App\Http\Requests\EventController\CheckIdRequest;
 
 class EventController extends Controller
 {
@@ -23,16 +26,19 @@ class EventController extends Controller
                     'start_date' => $request->post('startDate'),
                     'venue_id' => $request->post('venueId'),
                 ]);
-                $event->league()->attach($request->post('leagueId'));
+                if($request->post('leagueId'))
+                    $event->league()->attach($request->post('leagueId'));
 
+                /**
+                 * PERDON PROFE USE UN ELSE :(
+                 */
                 if($request->post('isIndividual')) {
                     $this->createIndividualEvent($request, $event->id);
-                    return back()->with('statusCreate', 'Event registered successfully.');
-                }
-                
-                $this->createTeamEvent($request, $event->id);
-                return back()->with('statusCreate', 'Event registered successfully.');
+                } else {
+                    $this->createTeamEvent($request, $event->id);
+                }    
             });
+            return back()->with('statusCreate', 'Event registered successfully.');
         } catch (QueryException $e) {
             return back()->with('statusCreate', 'Unable to create event right now.');
         }     
@@ -59,9 +65,53 @@ class EventController extends Controller
         }
     }
 
+    public function delete(CheckIdRequest $request)
+    {
+        Event::destroy($request->post('id'));
+        return back()->with([
+            'statusDelete' => 'Event deleted successfully.',
+            'deletedId' => $request->post('id')
+        ]);
+    }
+
+    public function restore(CheckIdRequest $request)
+    {
+        Event::withTrashed()
+            ->find($request->post('id'))
+            ->restore();
+        return back()->with('statusRestore', 'Event restored successfully.');
+    }
+
     public function show()
     {
-        return view('eventManagement')->with('events', Event::all());
+        return view('eventManagement', [
+            'events' => Event::all(),
+            'leagues' => League::all(),
+            'venues' => Venue::all()
+        ]);
+    }
+
+    public function edit($id)
+    {
+        $validation = $this->validateId($id);
+        if($validation !== true)
+            return back();
+
+        return view('eventUpdate', [
+            'event' => Event::find($id),
+            'leagues' => League::all(),
+            'venues' => Venue::all()
+        ]);
+    }
+
+    private function validateId($id)
+    {
+        $validation = Validator::make(['id' => $id], [
+            'id' => 'numeric|exists:events'
+        ]);
+        if($validation->fails())
+            return $validation;
+        return true;
     }
 
     private function createIndividualEvent($request, $eventId)
