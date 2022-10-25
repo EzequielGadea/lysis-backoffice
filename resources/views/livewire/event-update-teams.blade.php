2fc90d26916052f98b455dc37ded524e4ad2df32
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-24 22:37:18 -0300

    cambiar nombres de variables

--- resources/views/livewire/event-update-teams.blade.php
+++ resources/views/livewire/event-update-teams.blade.php
@@ -2,8 +2,8 @@
     <div class="flex flex-col gap-1">
         <label for="teamLocalId" class="font-medium text-zinc-700">Local team</label>
         <select name="teamLocalId" wire:model="chosenLocalTeam" id="teamLocalId" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner" autocomplete="off">
-            <option selected>Choose team</option>
-            @foreach ($localTeams as $team)
+            <option value="{{ $local->id }}"selected>{{ $local->name }}</option>
+            @foreach ($locals as $team)
                 <option value="{{ $team->id }}">{{ $team->name }}</option>
             @endforeach
         </select>
@@ -12,8 +12,8 @@
     <div class="flex flex-col gap-1">
         <label for="teamVisitorId" class="font-medium text-zinc-700">Visitor team</label>
         <select name="teamVisitorId" wire:model="chosenVisitorTeam" id="teamVisitorId" class="w-64 bg-slate-200 px-3 py-1 rounded-md placeholder:text-zinc-600 shadow-inner" autocomplete="off">
-            <option selected>Choose team</option>
-            @foreach ($visitorTeams as $team)
+            <option value="{{ $visitor->id }}"selected>{{ $visitor->name }}</option>
+            @foreach ($visitors as $team)
                 <option value="{{ $team->id }}">{{ $team->name }}</option>
             @endforeach
         </select>
