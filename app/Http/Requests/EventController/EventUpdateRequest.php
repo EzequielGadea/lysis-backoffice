8e740256d16d80fc592c945f348d814548896a33
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-24 22:44:34 -0300

    corregir reglas de validacion - formato de fecha ahora es correcto - leagueId ahora es required por cambios en la bd - correccion de typos y cambio de valores de if en isIndividual

--- app/Http/Requests/EventController/EventUpdateRequest.php
+++ app/Http/Requests/EventController/EventUpdateRequest.php
@@ -25,14 +25,14 @@ class EventUpdateRequest extends FormRequest
     {
         return [
             'eventId' => 'required|numeric|exists:events,id',
-            'startDate' => 'required|date',
+            'startDate' => 'required|date_format:"Y-m-d\TH:i"',
             'venueId' => 'required|numeric|exists:venues,id',
             'isIndividual' => 'required|boolean',
-            'leagueId' => 'exclude_if:leagueId,NULL|numeric|exists:leagues,id',
-            'playerVisitorId' => 'exclude_unless:isIndiviual,true|required|numeric|exists:players,id',
-            'playerLocalId' => 'exclude_unless:isIndiviual,true|required|numeric|exists:players,id',
-            'teamLocalId' => 'exclude_if:isIndividual,true|required|numeric|exists:teams,id',
-            'teamVisitorId' => 'exclude_if:isIndividual,true|required|numeric|exists:teams,id'
+            'leagueId' => 'required|numeric|exists:leagues,id',
+            'playerVisitorId' => 'exclude_unless:isIndividual,1|required|numeric|exists:players,id',
+            'playerLocalId' => 'exclude_unless:isIndividual,1|required|numeric|exists:players,id',
+            'teamLocalId' => 'exclude_if:isIndividual,1|required|numeric|exists:teams,id',
+            'teamVisitorId' => 'exclude_if:isIndividual,1|required|numeric|exists:teams,id'
         ];
     }
 }
