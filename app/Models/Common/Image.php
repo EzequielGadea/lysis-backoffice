3bffb4484fca6641865b5201217c9c23593685e3
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-26 21:13:20 -0300

    crear modelo para tabla images

---
+++ app/Models/Common/Image.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Common;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Image extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'file_name'
+    ];
+}
