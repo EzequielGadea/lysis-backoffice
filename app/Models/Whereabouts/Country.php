50a3e6ed1b1af59bd9075664738af176fb2e7fea
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-28 18:10:33 -0300

    eliminar relacion con Image y actualizar fillables

--- app/Models/Whereabouts/Country.php
+++ app/Models/Whereabouts/Country.php
@@ -2,16 +2,15 @@
 
 namespace App\Models\Whereabouts;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Common\League;
+use App\Models\Events\Referee;
 use App\Models\Players\Player;
 use App\Models\Teams\Team;
 use App\Models\Teams\Manager;
-use App\Models\Common\League;
-use App\Models\Common\Image;
-use App\Models\Events\Referee;
 use App\Models\Whereabouts\City;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Country extends Model
 {
@@ -19,7 +18,7 @@ class Country extends Model
 
     protected $fillable = [
         'name',
-        'image_id'
+        'picture'
     ];
 
     public function players()
@@ -50,10 +49,5 @@ class Country extends Model
     public function cities()
     {
         return $this->hasMany(City::class);
-    }
-    
-    public function image()
-    {
-        return $this->belongsTo(Image::class);
     }
 }
