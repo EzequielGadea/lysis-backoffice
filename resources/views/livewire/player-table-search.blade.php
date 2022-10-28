31c3ee5fe3c2dec1ea2bc2eb6072e5f8f38f8332
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-28 17:06:30 -0300

    agregar imagen del jugador al buscar

--- resources/views/livewire/player-table-search.blade.php
+++ resources/views/livewire/player-table-search.blade.php
@@ -7,8 +7,8 @@
         </div>
         <button class="bg-blue-600 text-white px-3 py-1 w-fit rounded-md" type="submit">Save</button>
     </div>
-    <div class="overflow-x-auto w-[30rem] rounded-md border border-slate-300">
-        <table class="table-fixed border-collapse whitespace-nowrap w-full">
+    <div class="overflow-x-auto w-[35rem] rounded-md border border-slate-300">
+        <table class="table-auto border-collapse whitespace-nowrap w-full">
             <thead>
                 <tr class="bg-slate-300">
                     <td class="p-3 font-light text-zinc-800">NAME</td>
@@ -19,7 +19,14 @@
             <tbody>
                 @foreach ($players as $player)
                     <tr class="border-b border-slate-300">
-                        <td class="p-3 text-zinc-800">{{ $player->name }} {{ $player->surname }}</td>
+                        <td class="p-3 text-zinc-800">
+                            <div class="flex flex-row items-center gap-4">
+                                <img src="{{ asset('images/' . $player->picture) }}" class="h-12 w-12 rounded-full shadow-sm">
+                                <p>
+                                    {{ $player->name }} {{ $player->surname }}
+                                </p>
+                            </div>
+                        </td>
                         <td class="p-3 text-zinc-800">{{ $player->country->name }}</td>
                         <td class="p-3 text-zinc-800 text-center w-1/5">
                             <input type="radio" name="playerId" id="playerId" value="{{ $player->id }}">
