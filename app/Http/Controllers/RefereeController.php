cb3def3ebcea31455310af70011455fa2e8ad073
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-27 21:01:13 -0300

    añadir soporte para fotos de perfil - actualizar reglas de validacion - agregar guardado de imagen a metodos update y create

--- app/Http/Controllers/RefereeController.php
+++ app/Http/Controllers/RefereeController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use Illuminate\Support\Collection;
 use App\Models\Events\Referee;
 use App\Models\Whereabouts\Country;
 
 class RefereeController extends Controller
 {
+    private $profilePicturesFolder = 'images';
+
     public function create(Request $request)
     {
         $validation = $this->validateCreation($request);
@@ -21,7 +24,8 @@ class RefereeController extends Controller
             'name' => $request->post('name'),
             'surname' => $request->post('surname'),
             'birth_date' => $request->post('birthDate'),
-            'country_id' => $request->post('countryId')
+            'country_id' => $request->post('countryId'),
+            'picture' => $this->storeProfilePicture($request->file('picture'))
         ]);
         return back()->with('statusCreate', 'Referee created succesfuly');
     }
@@ -44,12 +48,18 @@ class RefereeController extends Controller
         if($validation !== true)
             return back()->withErrors($validation);
 
-        Referee::find($request->post('id'))->update([
+        $referee = Referee::find($request->post('id'));
+        $referee->update([
             'name' => $request->post('name'),
             'surname' => $request->post('surname'),
             'birth_date' => $request->post('birthDate'),
             'country_id' => $request->post('countryId')
         ]);
+        if($request->file('picture') !== null)
+            $referee->update([
+                'picture' => $this->storeProfilePicture($request->file('picture'))
+            ]);
+
         return back()->with([
             'statusUpdate' => 'Referee updated succesfuly, you will soon be redirected.',
             'isRedirected' => 'true'
@@ -89,13 +99,22 @@ class RefereeController extends Controller
         ]);
     }
 
+    private function storeProfilePicture($file)
+    {
+        $fileName = Str::random(32) . '.' . $file->extension();
+        $file->move($this->profilePicturesFolder, $fileName);
+
+        return $fileName;
+    }
+
     private function validateCreation($request)
     {
         $validation = Validator::make($request->all(), [
             'name' => 'required|max:255',
             'surname' => 'required|max:255',
             'birthDate' => 'required|date|before:today',
-            'countryId' => 'required|exists:countries,id'
+            'countryId' => 'required|exists:countries,id',
+            'picture' => 'required|image|max:5000'
         ]);
         if($validation->fails())
             return $validation;
@@ -109,7 +128,8 @@ class RefereeController extends Controller
             'name' => 'required|max:255',
             'surname' => 'required|max:255',
             'birthDate' => 'required|date',
-            'countryId' => 'required|numeric|exists:countries,id'
+            'countryId' => 'required|numeric|exists:countries,id',
+            'picture' => 'nullable|image|max:5000'
         ]);
         if($validation->fails())
             return $validation;
