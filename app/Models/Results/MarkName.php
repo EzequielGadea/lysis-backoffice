db07680105e67a488968b24534ab42b939c706fc
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-31 22:32:12 -0300

    actualizar fillables y agregar relacion con crieteria

--- app/Models/Results/MarkName.php
+++ app/Models/Results/MarkName.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Results;
 
+use App\Models\Common\Criteria;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -12,11 +13,17 @@ class MarkName extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'name'
+        'name',
+        'criteria_id'
     ];
 
     public function marks()
     {
         return $this->hasMany(ByMark::class);
+    }
+
+    public function criteria()
+    {
+        return $this->belongsTo(Criteria::class);
     }
 }
