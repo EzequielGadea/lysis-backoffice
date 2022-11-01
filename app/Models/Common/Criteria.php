25ed0e49093fc917323848b515ae64c618ad6f95
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-10-31 22:30:49 -0300

    crear modelo para tabla criterias permite conocer si una marca debe ser ascendente o descendente

---
+++ app/Models/Common/Criteria.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Common;
+
+use App\Models\Results\MarkName;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Criteria extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function markNames()
+    {
+        return $this->hasMany(MarkName::class);
+    }
+}
