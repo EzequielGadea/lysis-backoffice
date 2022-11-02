dd62f38a61350f432173f54c6ad748840bbefc6c
Facundodra <112714035+Facundodra@users.noreply.github.com>
2022-11-02 00:54:35 -0300

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -32,6 +32,7 @@ class LoginController extends Controller
     public function logout() 
     {
         Auth::logout();
+        return redirect('/');
     }
 
     private function validateAuthentication($request) 
