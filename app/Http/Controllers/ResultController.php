5663a8439299a66f2c8b899cf7044d7bd0a1fbee
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-04 18:46:51 -0300

    esto probablemente se vaya en un commit futuro

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -4,15 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ResultRequest;
 use App\Models\Events\Event;
+use App\Models\Results\ByMarkEventPlayerTeam;
+use App\Models\Results\ByMarkPlayerLocal;
+use App\Models\Results\ByMarkPlayerVisitor;
 use App\Services\ByMarkService;
 use App\Services\ByPointService;
 use App\Services\BySetService;
+use Illuminate\Http\Request;
 
 class ResultController extends Controller
 {
-    public function create(ResultRequest $request, Event $event, ByMarkService $service)
+    private $service;
+
+    public function __construct(ByMarkService $service)
     {
-        $service->create($request, $event);
+        $this->service = $service;
+    }
+
+    public function create(ResultRequest $request, Event $event)
+    {
+        $this->service->create($request, $event);
         return back()->with('statusCreate', 'Result created successfully.');
     }
 
@@ -26,5 +37,25 @@ class ResultController extends Controller
                 'bySets' => '3'
             ]
         ]);
+    }
+
+    public function edit($mark, $role)
+    {
+        if ($role == 'visitor')
+            $playerMark = ByMarkPlayerVisitor::findOrFail($mark);
+        if ($role == 'local')
+            $playerMark = ByMarkPlayerLocal::findOrFail($mark);
+        return $this->service->edit($playerMark, $role);
+    }
+
+    public function update(Event $event, Request $request, $playerMark, $role)
+    {
+        if ($role == 'visitor')
+            $mark = ByMarkPlayerVisitor::find($playerMark);
+        if ($role == 'local')
+            $mark = ByMarkPlayerLocal::find($playerMark);
+        if ($role = 'teams')
+            $mark = ByMarkEventPlayerTeam::find($playerMark);
+        return $this->service->update($event, $mark, $request);
     }
 }
