502dda4e3b965c3fab067e44331ecadd98928348
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-04 14:06:21 -0300

    agregar imagenes de jugadores a las tablas

--- resources/views/components/results/by-mark-table.blade.php
+++ resources/views/components/results/by-mark-table.blade.php
@@ -20,7 +20,16 @@
                             <td class="p-3 text-zinc-800">{{ $result->markName->name }}</td>
                             <td class="p-3 text-zinc-800">{{ $mark->mark_value }}{{ $mark->result->markName->unit->unit }}</td>
                             <td class="p-3 text-zinc-800">{{ $result->markName->criteria->name }}</td>
-                            <td class="p-3 text-zinc-800">{{ $mark->player->name ?? $mark->playerVisitor->player->name }} {{ $mark->player->surname ?? $mark->playerVisitor->player->surname }}</td>
+                            <td class="p-3 text-zinc-800">
+                                <div class="flex flex-row gap-4 items-center">
+                                    <img src="{{ asset('images/' . $mark->playerVisitor->player->picture) }}" 
+                                         alt="{{ $mark->player->name ?? $mark->playerVisitor->player->name }} {{ $mark->player->surname ?? $mark->playerVisitor->player->surname }}"
+                                        class="h-16 w-16 rounded-full shadow-md bg-center"/>
+                                    <p>
+                                        {{ $mark->playerVisitor->player->name }} {{ $mark->playerVisitor->player->surname }}
+                                    </p>
+                                </div>
+                            </td>
                             <td class="p-3 text-zinc-800">
                                 @if ($mark->getTable() == 'by_mark_player_visitor')
                                     <p>Visitor</p>
@@ -65,7 +74,16 @@
                             <td class="p-3 text-zinc-800">{{ $result->markName->name }}</td>
                             <td class="p-3 text-zinc-800">{{ $mark->mark_value }}{{ $mark->result->markName->unit->unit }}</td>
                             <td class="p-3 text-zinc-800">{{ $result->markName->criteria->name }}</td>
-                            <td class="p-3 text-zinc-800">{{ $mark->player->name ?? $mark->playerLocal->player->name ?? 'no tiene' }} {{ $mark->player->surname ?? $mark->playerLocal->player->surname ?? 'no tiene' }}</td>
+                            <td class="p-3 text-zinc-800">
+                                <div class="flex flex-row gap-4 items-center">
+                                    <img src="{{ asset('images/' . $mark->playerLocal->player->picture) }}" 
+                                    alt="{{ $mark->playerLocal->player->name }} {{ $mark->playerLocal->player->surname }}"
+                                   class="h-16 w-16 rounded-full shadow-md bg-center"/>
+                                   <p>
+                                       {{ $mark->playerLocal->player->name }} {{ $mark->playerLocal->player->surname }}
+                                   </p>
+                                </div>
+                            </td>
                             <td class="p-3 text-zinc-800">
                                 @if ($mark->getTable() == 'by_mark_player_visitor')
                                     <p>Visitor</p>
@@ -80,57 +98,6 @@
                                     </form>
                                     <form action="/mark/local/delete/{{ $mark->id }}" method="post">
                                         @csrf
-                                        @method('delete')
-                                        <button class="font-semibold text-blue-600">Delete</button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    @else
-        <div class="rounded-md overflow-x-auto shadow-xl w-[50rem]">
-            <table class="table-auto border-collapse w-full whitespace-nowrap">
-                <thead>
-                    <tr class="bg-slate-300">
-                        <td class="p-3 font-light text-zinc-800">ID</td>
-                        <td class="p-3 font-light text-zinc-800">MARK NAME</td>
-                        <td class="p-3 font-light text-zinc-800">VALUE</td>
-                        <td class="p-3 font-light text-zinc-800">MARK CRITERIA</td>
-                        <td class="p-3 font-light text-zinc-800">PLAYER</td>
-                        <td class="p-3 font-light text-zinc-800">AS</td>
-                        <td class="p-3 font-light text-zinc-800 text-center">ACTIONS</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($result->marksPlayerTeam()->get()->sortBy([['mark_value', $result->markName->criteria->sort_by]]) as $mark)
-                        <tr class="border-b border-slate-300">
-                            <td class="p-3 text-zinc-800">{{ $mark->id }}</td>
-                            <td class="p-3 text-zinc-800">{{ $result->markName->name }}</td>
-                            <td class="p-3 text-zinc-800">{{ $mark->mark_value }}{{ $mark->result->markName->unit->unit }}</td>
-                            <td class="p-3 text-zinc-800">{{ $result->markName->criteria->name }}</td>
-                            <td class="p-3 text-zinc-800">{{ $mark->player->name ?? $mark->playerVisitor->player->name }} {{ $mark->player->surname ?? $mark->playerVisitor->player->surname }}</td>
-                            <td class="p-3 text-zinc-800">
-                                @if ($mark->getTable() == 'by_mark_player_visitor')
-                                    <p>Visitor</p>
-                                @else
-                                    <p>Local</p>
-                                @endif
-                            </td>
-                            <td class="p-3 text-zinc-800">
-                                <div class="flex flex-col items-center">
-                                    @if ($mark->getTable() == 'by_mark_player_visitor')
-                                        <form action="/result/update/{{ $result->id }}/role/visitor" method="GET">
-                                            <button class="font-semibold text-blue-600">Edit</button>
-                                        </form>
-                                    @else
-                                        <form action="/result/update/{{ $result->id }}/role/local" method="GET">
-                                            <button class="font-semibold text-blue-600">Edit</button>
-                                        </form>
-                                    @endif
-                                    <form action="/result/delete/{{ $mark->id }}" method="post">
                                         @method('delete')
                                         <button class="font-semibold text-blue-600">Delete</button>
                                     </form>
