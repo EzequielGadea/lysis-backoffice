7ca08bb780a81939806ed44552330b1713dd8543
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-04 18:45:48 -0300

    gestion de puntos en sets

--- resources/views/results/bySet/team/management.blade.php
+++ resources/views/results/bySet/team/management.blade.php
@@ -1,45 +1,51 @@
 <x-layout>
     <x-slot name="title">Updating score</x-slot>
     <div class="flex flex-col items-center pt-6 px-8 flex-grow">
-      <div class="flex flex-row justify-between w-full">
-        <p class="text-2xl text-zinc-800 font-semibold mb-6 w-full">Points</p>
-        <form action="/eventManagement" method="GET">
-            <button class="text-xl font-semibold underline text-zinc-800 hover:cursor-pointer">Back</button>
-        </form>
-      </div>
-      <div class="flex flex-col gap-12">
-          <div class="rounded-md overflow-x-auto shadow-xl w-[70rem]">
-              <table class="table-auto border-collapse w-full whitespace-nowrap">
-                  <thead>
-                      <tr class="bg-slate-300">
-                          <td class="p-3 font-light text-zinc-800">ID</td>
-                          <td class="p-3 font-light text-zinc-800">SET</td>
-                          <td class="p-3 font-light text-zinc-800">POINTS IN FAVOR</td>
-                          <td class="p-3 font-light text-zinc-800">POINTS AGAINST</td>
-                          <td class="p-3 font-light text-zinc-800">PLAYER</td>
-                          <td class="p-3 font-light text-zinc-800">TEAM</td>
-                          <td class="p-3 font-light text-zinc-800">MINUTE</td>
-                          <td class="p-3 font-light text-zinc-800">CREATED AT</td>
-                          <td class="p-3 font-light text-zinc-800">UPDATED AT</td>
-                          <td class="p-3 font-light text-zinc-800 text-center">ACTIONS</td>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      @foreach ($event->result()->allTeamPoints()->sortBy([['minute', 'asc']]) as $point)
-                              <tr class="border-b border-slate-300">
+        <div class="flex flex-row justify-between w-full">
+            <p class="text-2xl text-zinc-800 font-semibold mb-6 w-full">Points</p>
+            <form action="/eventManagement" method="GET">
+                <button class="text-xl font-semibold underline text-zinc-800 hover:cursor-pointer">Back</button>
+            </form>
+        </div>
+        <div class="flex flex-col gap-12">
+            <div class="rounded-md overflow-x-auto shadow-xl w-[75rem]">
+                <table class="table-auto border-collapse w-full whitespace-nowrap">
+                    <thead>
+                        <tr class="bg-slate-300">
+                            <td class="p-3 font-light text-zinc-800">ID</td>
+                            <td class="p-3 font-light text-zinc-800">SET</td>
+                            <td class="p-3 font-light text-zinc-800">MIN</td>
+                            <td class="p-3 font-light text-zinc-800">POINTS</td>
+                            <td class="p-3 font-light text-zinc-800">PLAYER</td>
+                            <td class="p-3 font-light text-zinc-800">TEAM</td>
+                            <td class="p-3 font-light text-zinc-800">CREATED AT</td>
+                            <td class="p-3 font-light text-zinc-800">UPDATED AT</td>
+                            <td class="p-3 font-light text-zinc-800 text-center">ACTIONS</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($event->result()->teamPoints()->get()->sortBy([['set.number', 'asc'], ['minute', 'desc']]) as $point)
+                            <tr class="border-b border-slate-300">
                                 <td class="p-3 text-zinc-800">{{ $point->id }}</td>
-                                <td class="p-3 text-zinc-800">{{ $point->set->number }}</td>
-                                <td class="p-3 text-zinc-800">{{ $point->points_in_favor }}</td>
-                                <td class="p-3 text-zinc-800">{{ $point->points_against }}</td>
+                                <td class="p-3 text-zinc-800">Set {{ $point->set->number }}</td>
+                                <td class="p-3 text-zinc-800">{{ $point->minute }}'</td>
+                                <td @class([
+                                    'p-3',
+                                    'font-semibold',
+                                    'text-green-600' => $point->points_in_favor !== 0,
+                                    'text-red-600' => $point->points_against !== 0,
+                                ])>
+                                    {{ $point->points_in_favor == 0 ? $point->points_against . ' against' : $point->points_in_favor . ' in favor' }}
+                                </td>
                                 <td class="p-3 text-zinc-800">
                                     <div class="flex flex-row gap-4 items-center">
-                                        <div style="background-image: url({{ asset('images/' . $point->eventPlayerTeam->playerTeam->player->picture) }})" 
+                                        <div style="background-image: url({{ asset('images/' . $point->eventPlayerTeam->playerTeam->player->picture) }})"
                                             class="h-16 w-16 rounded-full shadow-md bg-center bg-contain 
                                             whitespace-nowrap overflow-hidden bg-no-repeat bg-origin-border 
-                                            bg-clip-padding"
-                                        ></div>
+                                            bg-clip-padding">
+                                        </div>
                                         <p>
-                                            {{ $point->eventPlayerTeam->playerTeam->player->name ?? '' }} 
+                                            {{ $point->eventPlayerTeam->playerTeam->player->name ?? '' }}
                                             {{ $point->eventPlayerTeam->playerTeam->player->surname }}
                                         </p>
                                     </div>
@@ -55,35 +61,31 @@
                                 </td>
                                 <td class="p-3 text-zinc-800">
                                     <div class="flex flex-col items-center">
-                                    <form action="/mark/team/edit/{{ $point->id }}" method="GET">
-                                        <button class="font-semibold text-blue-600">Edit</button>
-                                    </form>
-                                    <form action="/mark/team/delete/{{ $point->id }}" method="post">
-                                        @csrf
-                                        @method('delete')
-                                        <button class="font-semibold text-blue-600">Delete</button>
-                                    </form>
+                                        <form action="/set/team/edit/{{ $point->id }}" method="GET">
+                                            <button class="font-semibold text-blue-600">Edit</button>
+                                        </form>
+                                        <form action="/set/team/delete/{{ $point->id }}" method="post">
+                                            @csrf
+                                            @method('delete')
+                                            <button class="font-semibold text-blue-600">Delete</button>
+                                        </form>
                                     </div>
                                 </td>
-                              </tr>
-                          @endforeach
-                  </tbody>
-              </table>
-          </div>
-      </div>
-      @if (session('statusDelete') && session('deletedId'))
-          <x-status-delete-get>
-              <x-slot name="action">/mark/team/restore/{{ session('deletedId') }}</x-slot>
-          </x-status-delete-get>
-      @endif
-  
-      @if (session('statusRestore'))
-          <x-status-restore />
-      @endif
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        @if (session('statusDelete') && session('deletedId'))
+            <x-status-delete-get>
+                <x-slot name="action">/set/team/restore/{{ session('deletedId') }}</x-slot>
+            </x-status-delete-get>
+        @endif
+
+        @if (session('statusRestore'))
+            <x-status-restore />
+        @endif
     </div>
-    @livewire('result.by-mark.create-team-mark', [
-        'result' => $event->result()->id,
-        'markName' => $event->result()->markName->name
-    ])
-  </x-layout>
-  
+    @livewire('result.by-set.create-team-set-point', ['result' => $event->result()])
+</x-layout>
