c44168e471185aafae3f58b37821237dee3c56c7
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-04 15:09:39 -0300

    crear componente para creacion de punto en set por equipos

---
+++ app/Http/Livewire/Result/BySet/CreateTeamSetPoint.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Result\BySet;
+
+use Livewire\Component;
+
+class CreateTeamSetPoint extends Component
+{
+    public function render()
+    {
+        return view('livewire.result.by-set.create-team-set-point');
+    }
+}
