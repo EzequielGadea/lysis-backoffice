36691636ca3c2c8567b871c318e98b6fde17dd5a
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-10 20:14:17 -0300

    regla de validacion para sets

--- app/Http/Requests/Sanctions/Assignment/Individual/CardlessSanctionCreateRequest.php
+++ app/Http/Requests/Sanctions/Assignment/Individual/CardlessSanctionCreateRequest.php
@@ -27,6 +27,13 @@ class CardlessSanctionCreateRequest extends FormRequest
     {
         return [
             'sanction' => 'required|integer|exists:sanction_cardlesses,id',
+            'set' => [
+                'nullable',
+                'integer',
+                Rule::exists('sets')->where(function ($query) {
+                    $query->where('event_id', $this->route('event')->result()->id);
+                }),
+            ],
             'player' => [
                 'bail',
                 'required',
