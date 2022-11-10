6be6617ecfdb096c37318de7897a08418fa91509
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-10 15:53:19 -0300

    validacion para asignacion de sancion sin tarjeta

---
+++ app/Http/Requests/Sanctions/Assignment/Individual/CardlessSanctionCreateRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Sanctions\Assignment\Individual;
+
+use App\Rules\IsPlayerLocalOrVisitor;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CardlessSanctionCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'sanction' => 'required|integer|exists:sanction_cardlesses,id',
+            'player' => [
+                'bail',
+                'required',
+                'integer',
+                'exists:players,id',
+                new IsPlayerLocalOrVisitor,
+            ],
+            'minute' => [
+                'required',
+                'integer',
+                'min:0',
+                'min:999',
+                Rule::unique('player_local_sanction_cardless')->where(function ($query) {
+                    $query->where('event_id', $this->route('event')->id);
+                }),
+                Rule::unique('player_visitor_sanction_cardless')->where(function ($query) {
+                    $query->where('event_id', $this->route('event')->id);
+                }),
+            ]
+        ];
+    }
+}
