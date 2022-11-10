ee774c6451e9c4dacf9e611a0f53d63e2da1d39b
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-10 15:40:38 -0300

    validacion para crear sanciones por tarjeta

---
+++ app/Http/Requests/Sanctions/Assignment/Individual/CardSanctionCreateRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Sanctions\Assignment\Individual;
+
+use App\Rules\IsPlayerLocalOrVisitor;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CardSanctionCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'sanction' => 'required|integer|exists:sanction_cards,id',
+            'player' => [
+                'bail',
+                'required',
+                'integer',
+                'exists:players,id',
+                new IsPlayerLocalOrVisitor,
+            ],
+            'minute' => [
+                'required',
+                'integer',
+                'min:0',
+                'max:999',
+                Rule::unique('player_local_sanction_card')->where(function ($query) {
+                    $query->where('event_id', $this->route('event')->id);
+                }),
+                Rule::unique('player_visitor_sanction_card')->where(function ($query) {
+                    $query->where('event_id', $this->route('event')->id);
+                }),
+            ],
+        ];
+    }
+}
