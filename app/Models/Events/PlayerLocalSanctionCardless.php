0ca8edabad7f3bc93f44cd5148c476a4b6dacd69
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-14 22:11:31 -0300

    crear relacion con Event

--- app/Models/Events/PlayerLocalSanctionCardless.php
+++ app/Models/Events/PlayerLocalSanctionCardless.php
@@ -5,6 +5,7 @@ namespace App\Models\Events;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Events\Event;
 use App\Models\Events\PlayerLocalSanctionCardlessSet;
 use App\Models\Sanctions\SanctionCardless;
 use App\Models\Players\PlayerLocal;
@@ -33,5 +34,9 @@ class PlayerLocalSanctionCardless extends Model
 
     public function inSet() {
         return $this->hasOne(PlayerLocalSanctionCardlessSet::class);
+    }
+
+    public function event() {
+        return $this->belongsTo(Event::class, 'event_id', 'id');
     }
 }
