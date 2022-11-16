29f3204f537269fc1e355c4d246510ee84844c2a
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-16 00:55:18 -0300

    eliminar validaciones para minutos

--- app/Http/Requests/Sanctions/Assignment/Individual/CardlessSanctionUpdateRequest.php
+++ app/Http/Requests/Sanctions/Assignment/Individual/CardlessSanctionUpdateRequest.php
@@ -38,12 +38,6 @@ class CardlessSanctionUpdateRequest extends FormRequest
                 'integer',
                 'min:0',
                 'max:999',
-                Rule::unique('player_local_sanction_cardless')->where(function ($query) {
-                    $query->where('event_id', $this->route('sanction')->event_id);
-                })->ignore($this->route('sanction')),
-                Rule::unique('player_visitor_sanction_cardless')->where(function ($query) {
-                    $query->where('event_id', $this->route('sanction')->event_id);
-                })->ignore($this->route('sanction')),
             ]
         ];
     }
