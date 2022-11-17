41e26f8f0ac7ae8578bac1d0e3c6686cb0c71066
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-16 23:16:35 -0300

    Fix: typos en use

--- app/Models/Events/EventPlayerTeamSanctionCard.php
+++ app/Models/Events/EventPlayerTeamSanctionCard.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Events\EventPlayerTeam;
-use App\Models\Events\EventPlayerTeamSanctionCardSet.php
+use App\Models\Events\EventPlayerTeamSanctionCardSet;
 use App\Models\Sanctions\SanctionCard;
 
 class EventPlayerTeamSanctionCard extends Model
