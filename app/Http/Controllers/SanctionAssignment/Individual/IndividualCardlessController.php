edb1a7dc4c6f166038afc8608969e3e8e2bb717b
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-16 22:58:24 -0300

    refactor: actualizar namespace por reordenen en carpetas

--- app/Http/Controllers/SanctionAssignment/Individual/IndividualCardlessController.php
+++ app/Http/Controllers/SanctionAssignment/Individual/IndividualCardlessController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\SanctionAssignment;
+namespace App\Http\Controllers\SanctionAssignment\Individual;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\QueryException;
