f224759443f32fc4e37dc19b7942b30ce884a309
Ezequiel Gadea <ezequielgadea10@gmail.com>
2022-11-16 23:12:46 -0300

    Fix: agregar use de facade Rule.

--- app/Http/Requests/Sanctions/Assignment/Team/CardSanctionUpdateRequest.php
+++ app/Http/Requests/Sanctions/Assignment/Team/CardSanctionUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests\Sanctions\Assignment\Team;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class CardSanctionUpdateRequest extends FormRequest
 {
